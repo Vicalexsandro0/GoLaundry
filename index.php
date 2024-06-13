5e57b395a31464e5d84356362cbe038cbe4e9281
StanleyJonathanITBSS <stanley.jonathan@itbss.ac.id>
2024-06-13 22:37:12 +0700

    Commit index

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Go Laundry</title>
+    <link rel="stylesheet" href="css/home.css">
+    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <section class="header">
+
+    <a href="index.php" class = "logo"><img src="assets/logo GO.png" alt=""></a>
+
+    <nav class=""navbar>
+        <a href="index.php">home</a>
+        <a href="index.php">kontak</a>
+        <a href="index.php">member</a>
+        <a href="index.php">tentang kami</a>
+    </nav>
+    </section>
+</body>
+</html>
