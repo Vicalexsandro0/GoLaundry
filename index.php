252dd4213c8f86818d51e45e955cf113c0010059
StanleyJonathanITBSS <stanley.jonathan@itbss.ac.id>
2024-06-18 15:42:32 +0700

    Index update

--- index.php
+++ index.php
@@ -5,22 +5,62 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Go Laundry</title>
     <link rel="stylesheet" href="css/home.css">
-    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 </head>
 <body>
     <section class="header">
 
-    <a href="index.php" class = "logo"><img src="assets/logo GO.png" alt=""></a>
+    <a href="index.php" class = "logo"><img src="assets/Group 2.png" alt="GoLaundry"></a>
 
-    <nav class=""navbar>
-        <a href="index.php">home</a>
-        <a href="index.php">kontak</a>
-        <a href="index.php">member</a>
-        <a href="index.php">tentang kami</a>
+    <nav class="navbar">
+        <a href="index.php">beranda</a>
+        <a href="contact.php">kontak</a>
+        <a href="member.php">member</a>
+        <a href="about.php">tentang kami</a>
     </nav>
 
     <div id="menu-btn" class="fas fa-bars"></div>
 
     </section>
+
+    <section class="services">
+        <h1 class="heading-title">jasa kami</h1>
+
+        <div class="box-container">
+            <div class="box">
+                <img src="" alt="">
+                <h3>laundry kiloan</h3>
+            </div>
+        </div>
+        <div class="box-container">
+            <div class="box">
+                <img src="" alt="">
+                <h3>cuci sepatu</h3>
+            </div>
+        </div>
+        <div class="box-container">
+            <div class="box">
+                <img src="" alt="">
+                <h3>laundry kiloan</h3>
+            </div>
+        </div>
+        <div class="box-container">
+            <div class="box">
+                <img src="" alt="">
+                <h3>cuci karpet</h3>
+            </div>
+        </div>
+        <div class="box-container">
+            <div class="box">
+                <img src="" alt="">
+                <h3>laundry satuan</h3>
+            </div>
+        </div>
+    </section>
+
+
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+<script src="js/script.js"></script>
 </body>
 </html>
