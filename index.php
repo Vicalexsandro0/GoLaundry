0b2e8c9bb41e73b7993b47e951270408b2c1233c
StanleyJonathanITBSS <stanley.jonathan@itbss.ac.id>
2024-06-21 00:31:36 +0700

    Added footer

--- index.php
+++ index.php
@@ -59,7 +59,24 @@
         </div>
     </section>
 
+    <section class="footer">
+        <div class="box-container">
+            <div class="box">
+                <h3>menu</h3>
+                <a href="index.php"><i class="fas fa-angle-right"></i>beranda</a>
+                <a href="contact.php"><i class="fas fa-angle-right"></i>kontak</a>
+                <a href="member.php"><i class="fas fa-angle-right"></i>member</a>
+                <a href="about.php"><i class="fas fa-angle-right"></i>tentang kami</a>
+            </div>
 
+            <div class="box">
+                <h3>contact us</h3>
+                <a href="https://wa.me/628999826888"><i class="fab fa-instagram"></i>GoLaundryPtk</a>
+                <a href="https://wa.me/628999826888"><i class="fas fa-phone"></i>+62-899-982-6888</a>
+                <a href="#"><i class="fas fa-map"></i>Jl. Budi Karya No. B3 </a>
+            </div>
+        </div>
+    </section>    
 <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
 <script src="js/script.js"></script>
 </body>
