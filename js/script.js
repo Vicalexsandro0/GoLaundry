2f618d8e5a467857e4a743cd997367b149fd2619
StanleyJonathanITBSS <168812347+StanleyJonathanITBSS@users.noreply.github.com>
2024-07-06 18:44:26 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -27,3 +27,51 @@ var swiper = new Swiper(".home-slider",{
         prevEl: ".swiper-button-prev"
     }
 })
+    
+function validateForm() {
+    const form = document.querySelector('.member-form');
+    let isValid = true;
+
+    form.querySelectorAll('input[required]').forEach(input => {
+        const errorMessage = input.nextElementSibling;
+
+        if (!input.validity.valid) {
+            isValid = false;
+            if (input.validity.valueMissing) {
+                errorMessage.textContent = `${input.previousElementSibling.textContent} harus diisi.`;
+            } else if (input.type === 'number' && input.validity.patternMismatch) {
+                errorMessage.textContent = `${input.previousElementSibling.textContent} harus berupa angka.`;
+            } else if (input.type === 'email' && input.validity.typeMismatch) {
+                errorMessage.textContent = `${input.previousElementSibling.textContent} harus berupa email yang valid.`;
+            } else if (input.type === 'password' && input.name === 'confirm_password') {
+                const passwordInput = form.querySelector('input[name="password"]');
+                if (input.value !== passwordInput.value) {
+                    errorMessage.textContent = `Password tidak cocok.`;
+                } else {
+                    errorMessage.textContent = '';
+                }
+            }
+        } else if (input.name === 'nik') {
+            // Specific validation for NIK
+            if (input.value.length !== 16) {
+                isValid = false;
+                errorMessage.textContent = "NIK harus tepat 16 digit.";
+            } else {
+                errorMessage.textContent = "";
+            }
+        } else {
+            errorMessage.textContent = '';
+        }
+    });
+
+    return isValid;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.querySelector('.member-form');
+    form.addEventListener('submit', function(event) {
+        if (!validateForm()) {
+            event.preventDefault(); // Prevent form submission if validation fails
+        }
+    });
+});
