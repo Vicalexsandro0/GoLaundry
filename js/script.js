5164be3222f5cc758f00839c09a18c27622d598c
Vanderhan <167097398+Vanderhan@users.noreply.github.com>
2024-07-06 22:22:46 +0700

    member

--- js/script.js
+++ js/script.js
@@ -27,3 +27,26 @@ var swiper = new Swiper(".home-slider",{
         prevEl: ".swiper-button-prev"
     }
 })
+
+// Awal Lonceng Notifikasi
+
+document.addEventListener('DOMContentLoaded', function() {
+    const notifIcon = document.getElementById('notif-icon');
+    const promoDropdown = document.getElementById('promo-dropdown');
+    const closeBtn = document.querySelector('.close-btn');
+
+    notifIcon.addEventListener('click', function() {
+        promoDropdown.style.display = 'block';
+    });
+
+    closeBtn.addEventListener('click', function() {
+        promoDropdown.style.display = 'none';
+    });
+
+    window.addEventListener('click', function(event) {
+        if (event.target !== notifIcon && !promoDropdown.contains(event.target)) {
+            promoDropdown.style.display = 'none';
+        }
+    });
+});
+// Akhir Lonceng Notifikasi
